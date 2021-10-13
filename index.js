a507f6451149b30b06a2682ad74d341cd1246740
Omar Amen <o.amen@ts.local>
2021-10-13 16:06:39 +0200

    make it better

---
+++ index.js
@@ -0,0 +1,16 @@
+const app = require('./app');
+
+const fetch = require("node-fetch");
+app.listen(443, ()=>{
+  console.log('Running server');
+ // console.log(app);
+ // console.log(app.storeA);
+})
+
+
+
+
+
+
+
+
